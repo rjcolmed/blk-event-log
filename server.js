f65ec94e90a8a406d7e43490265a5ec4eb15a8ee
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-03-26 21:58:56 -0400

    refactor web services calls and related variables for use with incoming form data

--- server.js
+++ server.js
@@ -2,48 +2,79 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const axios = require('axios')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 const port = process.env.port || '3002'
 
-//VARIABLES
-const username = process.env.USERNAME
-const password = process.env.PASSWORD
-const deviceId = process.env.DEVICE_ID
-const appType = process.env.APP_TYPE
-const deviceDetails = `{
-                "AppVersion": "1",
-                "DeviceId": "${deviceId}",
-                "DeviceNickname": "Test Device",
-                "OSVersion": "1",
-                "HasBarcodeScanner": "false",
-                "AppType": "${appType}",
-                "Device": "1",
-                "Platform": "1"
-}`
-
 const baseUrl = process.env.BASE_URL
-const getBuildingsPath = `/?u=${username}&p=${password}&t=${appType}&d=${deviceId}&format=json`
 
 //ROUTES
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+  const deviceId = req.body.deviceId
+  const getBuildingsPath = `/?u=${username}&p=${password}&t=2&d=${deviceId}&format=json`
+
+  let deviceDetails = `{
+    "AppVersion": "1",
+    "DeviceId": "${deviceId}",
+    "DeviceNickname": "Test Device",
+    "OSVersion": "1",
+    "HasBarcodeScanner": "false",
+    "AppType": "2",
+    "Device": "1",
+    "Platform": "1"
+  }`
 
   axios.get(`${baseUrl}${getBuildingsPath}`)
     .then(response => {
       const bldg = response.data.Buildings[0]
 
-      console.log(deviceDetails)
-
       const occupanciesPath = `/Buildings/${bldg.Id}/Occupancies?u=${username}&p=${password}&l=${bldg.LoginId}&d=${deviceId}&format=json`
       const eventTypesPath = `/Buildings/${bldg.Id}/EventLogTypes?u=${username}&p=${password}&l=${bldg.LoginId}&d=${deviceId}&format=json`
       const eventsPath = `/Buildings/${bldg.Id}/Events?u=${username}&p=${password}&l=${bldg.LoginId}&d=${deviceId}&format=json`
 
-      console.log(bldg.DeviceStatus)
+      if ( bldg.DeviceStatus === 0 ) {
+        const authDevicePath = `/Auth?u=${username}&p=${password}&l=${bldg.LoginId}&format=json`
+        
+        return axios({
+            method: 'post',
+            url: `${baseUrl}${authDevicePath}`,
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            data: deviceDetails
+          })
+          .then(results => {
+            return getEventsByUnit(baseUrl, eventsPath, occupanciesPath, eventTypesPath, res)
+          })
+          .catch(err => console.log('Device authorization failed: ', err))
+      } else {
+        getEventsByUnit(baseUrl, eventsPath, occupanciesPath, eventTypesPath, res)
+      }
+  })
+  .catch(err => console.log(err))
+})
 
-    return axios.get(`${baseUrl}${eventsPath}`)
+app.get('/api/events', (req, res) => {
+  res.json(app.locals.events)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
+})
+
+const getEventsByUnit = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res) => {
+  return axios.get(`${baseUrl}${eventsPath}`)
     .then(allEvents => {
 
       return allEvents.data.map(event => {
@@ -102,18 +133,4 @@ app.get('/', (req, res) => {
       .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
-  })
-  .catch(err => console.log(err))
-})
-
-app.get('/auth', (req, res) => {
-  res.render('index.html')
-})
-
-app.get('/api/events', (req, res) => {
-  res.json(app.locals.events)
-})
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}`)
-})
+}
