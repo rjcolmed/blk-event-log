e901a8afc9a02f693f8ebef5987b5a02ecbc5c92
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-04-06 13:13:39 -0400

    refactor to reshape the data sent from api endpoint

--- web_services/getEventsByUnit.js
+++ web_services/getEventsByUnit.js
@@ -3,7 +3,6 @@ const axios = require('axios')
 module.exports = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res, app) => {
   return axios.get(`${baseUrl}${eventsPath}`)
     .then(allEvents => {
-
       return allEvents.data.map(event => {
         return { 
           OccupancyId: event.OccupancyId,
@@ -18,53 +17,39 @@ module.exports = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res, app
           allOccupancies.data.forEach(occupancy => {
             if ( occupancy.Id === newEvent.OccupancyId ) {
               newEvent.Number = occupancy.Number
+              delete newEvent.OccupancyId
             }
           })
         })
-        
+
         return newEventsList.filter(event => event.Number )
       })
       .then(newUnitEventsList => {
-        axios.get(`${baseUrl}${eventTypesPath}`)
-        .then(allEventTypes => {
-          allEventTypes.data.forEach(eventType => {
-            newUnitEventsList.forEach(newUnitEvent => {
-              if ( eventType.Id === newUnitEvent.TypeId ) {
-                newUnitEvent.ShortDescription = eventType.ShortDescription
+        
+        return newUnitEventsList.map(el => el.Number)
+          .filter((num, idx, arr) => arr.indexOf(num) === idx)
+          .map(aptNum => {
+            return {
+              'Apartment': aptNum,
+              'Items': []
+            }
+          })
+          .map(unit => {
+            newUnitEventsList.map(el => {
+              if ( unit.Apartment === el.Number ) {
+                unit.Items.push(el.TypeId)
               }
-            })
+
+                return el
+              })
+
+              return unit
           })
+      })
+      .then(eventsByUnit => {
+        app.locals.events = eventsByUnit
   
-          return [...newUnitEventsList].sort((a, b) => {
-            return a.Number.localeCompare(b.Number)
-          })
-        })
-        .then(newUnitEventsTypesList => {
-          return newUnitEventsTypesList.reduce((acc, nextEvent) => {
-            delete nextEvent.OccupancyId
-            delete nextEvent.TypeId
-
-            if ( acc.hasOwnProperty(nextEvent.Number) ) {
-              acc[nextEvent.Number].events.push(nextEvent)
-              acc[nextEvent.Number].count++
-            } else {
-              acc[nextEvent.Number] = { events: [nextEvent], count: 1 }
-            }
-
-            return acc
-          }, {})
-        })
-        .then(eventsByUnit => {
-          const groupedEvents = []
-
-          for (const key in eventsByUnit) {
-            if ( eventsByUnit.hasOwnProperty(key) ) {
-              groupedEvents.push(eventsByUnit[key])
-            }
-          }
-          app.locals.events = groupedEvents
-          res.send('Events ready to be consumed at /api/events')
-        })
+        res.send('Events ready to be consumed at /api/events')
       })
       .catch(err => console.log(err))
     })
