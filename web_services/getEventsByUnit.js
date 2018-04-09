fe5ecaf94e23a8898cbd9895f169c844280d4d2c
Ricardo Colon <Ricardo@buildinglink.com>
2018-04-09 15:52:53 -0400

    wip - re-shaping data to include all units, with or without events

--- web_services/getEventsByUnit.js
+++ web_services/getEventsByUnit.js
@@ -1,57 +1,86 @@
 const axios = require('axios')
 
 module.exports = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res, app) => {
-  return axios.get(`${baseUrl}${eventsPath}`)
-    .then(allEvents => {
-      return allEvents.data.map(event => {
-        return { 
-          OccupancyId: event.OccupancyId,
-          TypeId: event.TypeId
+
+  axios.get(`${baseUrl}${occupanciesPath}`)
+    .then(allOccupancies => {
+      return allOccupancies.data.map(occupancy => {
+        return {
+          Id: occupancy.Id,
+          Number: occupancy.Number,
+          Events: []
         }
       })
     })
-    .then(newEventsList => {
-      axios.get(`${baseUrl}${occupanciesPath}`)
-      .then(allOccupancies => {
-        newEventsList.forEach(newEvent => {
-          allOccupancies.data.forEach(occupancy => {
-            if ( occupancy.Id === newEvent.OccupancyId ) {
-              newEvent.Number = occupancy.Number
-              delete newEvent.OccupancyId
-            }
-          })
-        })
+    .then(justIdsAndNumbers => {
+      // console.log(justIdsAndNumbers)
 
-        return newEventsList.filter(event => event.Number ) //filter out events whose unot/apt number is undefined
-      })
-      .then(newUnitEventsList => {
-        return newUnitEventsList.map(el => el.Number)
-          .filter((num, idx, arr) => arr.indexOf(num) === idx)
-          .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-          .map(aptNum => {
-            return {
-              'Apartment': aptNum,
-              'Items': []
-            }
-          })
-          .map(unit => {
-            newUnitEventsList.map(el => {
-              if ( unit.Apartment === el.Number ) {
-                unit.Items.push(el.TypeId)
+      axios.get(`${baseUrl}${eventsPath}`)
+        .then(allEvents => {
+          justIdsAndNumbers.forEach(idAndNumber => {
+            return allEvents.data.forEach(event => {
+              if ( idAndNumber.Id === event.OccupancyId ) {
+                idAndNumber.Events.push(event.TypeId)
               }
-
-                return el
-              })
-
-              return unit
+            })
           })
-      })
-      .then(eventsByUnit => {
-        app.locals.events = eventsByUnit
-  
-        res.send('Events ready to be consumed at /api/events')
-      })
-      .catch(err => console.log(err))
+
+          return justIdsAndNumbers
+        })
+        .then(results => console.log(results))
     })
     .catch(err => console.log(err))
+  // return axios.get(`${baseUrl}${eventsPath}`)
+  //   .then(allEvents => {
+  //     return allEvents.data.map(event => {
+  //       return { 
+  //         OccupancyId: event.OccupancyId,
+  //         TypeId: event.TypeId
+  //       }
+  //     })
+  //   })
+  //   .then(newEventsList => {
+  //     axios.get(`${baseUrl}${occupanciesPath}`)
+  //     .then(allOccupancies => {
+  //       newEventsList.forEach(newEvent => {
+  //         allOccupancies.data.forEach(occupancy => {
+  //           if ( occupancy.Id === newEvent.OccupancyId ) {
+  //             newEvent.Number = occupancy.Number
+  //             delete newEvent.OccupancyId
+  //           }
+  //         })
+  //       })
+
+  //       return newEventsList.filter(event => event.Number ) //filter out events whose unot/apt number is undefined
+  //     })
+  //     .then(newUnitEventsList => {
+  //       return newUnitEventsList.map(el => el.Number)
+  //         .filter((num, idx, arr) => arr.indexOf(num) === idx)
+  //         .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+  //         .map(aptNum => {
+  //           return {
+  //             'Apartment': aptNum,
+  //             'Items': []
+  //           }
+  //         })
+  //         .map(unit => {
+  //           newUnitEventsList.map(el => {
+  //             if ( unit.Apartment === el.Number ) {
+  //               unit.Items.push(el.TypeId)
+  //             }
+
+  //               return el
+  //             })
+
+  //             return unit
+  //         })
+  //     })
+  //     .then(eventsByUnit => {
+  //       app.locals.events = eventsByUnit
+  
+  //       res.send('Events ready to be consumed at /api/events')
+  //     })
+  //     .catch(err => console.log(err))
+  //   })
+  //   .catch(err => console.log(err))
 }
