b4755f812c48f5a18df590b62bee784585792302
Ricardo Colon <Ricardo@buildinglink.com>
2018-04-09 15:52:07 -0400

    use cors

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 const getEventsByUnit = require('./web_services/getEventsByUnit')
 const getBuildings = require('./web_services/getBuildings')
+const cors = require('cors')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 
 const port = process.env.port || '3002'
 const baseUrl = process.env.BASE_URL
