b8fd3b6f69537e3d00690e3af3a34c4e289facd5
[Ricardo Colon-Medina] <[rjcolmed@gmail.com]>
2018-04-09 22:01:29 -0400

    finish re-shaping data so that it includes units without events

--- web_services/getEventsByUnit.js
+++ web_services/getEventsByUnit.js
@@ -13,8 +13,6 @@ module.exports = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res, app
       })
     })
     .then(justIdsAndNumbers => {
-      // console.log(justIdsAndNumbers)
-
       axios.get(`${baseUrl}${eventsPath}`)
         .then(allEvents => {
           justIdsAndNumbers.forEach(idAndNumber => {
@@ -25,62 +23,18 @@ module.exports = (baseUrl, eventsPath, occupanciesPath, eventTypesPath, res, app
             })
           })
 
-          return justIdsAndNumbers
+          return justIdsAndNumbers.map(eventUnit => {
+            delete eventUnit.Id
+            return eventUnit
+          })
         })
-        .then(results => console.log(results))
+        .then(sortedEventsByUnits => {
+          app.locals.events = sortedEventsByUnits
+
+          res.send('Events ready to be consumed at /api/events')
+        })
+        .catch(err => console.log(err))
     })
     .catch(err => console.log(err))
-  // return axios.get(`${baseUrl}${eventsPath}`)
-  //   .then(allEvents => {
-  //     return allEvents.data.map(event => {
-  //       return { 
-  //         OccupancyId: event.OccupancyId,
-  //         TypeId: event.TypeId
-  //       }
-  //     })
-  //   })
-  //   .then(newEventsList => {
-  //     axios.get(`${baseUrl}${occupanciesPath}`)
-  //     .then(allOccupancies => {
-  //       newEventsList.forEach(newEvent => {
-  //         allOccupancies.data.forEach(occupancy => {
-  //           if ( occupancy.Id === newEvent.OccupancyId ) {
-  //             newEvent.Number = occupancy.Number
-  //             delete newEvent.OccupancyId
-  //           }
-  //         })
-  //       })
-
-  //       return newEventsList.filter(event => event.Number ) //filter out events whose unot/apt number is undefined
-  //     })
-  //     .then(newUnitEventsList => {
-  //       return newUnitEventsList.map(el => el.Number)
-  //         .filter((num, idx, arr) => arr.indexOf(num) === idx)
-  //         .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
-  //         .map(aptNum => {
-  //           return {
-  //             'Apartment': aptNum,
-  //             'Items': []
-  //           }
-  //         })
-  //         .map(unit => {
-  //           newUnitEventsList.map(el => {
-  //             if ( unit.Apartment === el.Number ) {
-  //               unit.Items.push(el.TypeId)
-  //             }
-
-  //               return el
-  //             })
-
-  //             return unit
-  //         })
-  //     })
-  //     .then(eventsByUnit => {
-  //       app.locals.events = eventsByUnit
   
-  //       res.send('Events ready to be consumed at /api/events')
-  //     })
-  //     .catch(err => console.log(err))
-  //   })
-  //   .catch(err => console.log(err))
 }
